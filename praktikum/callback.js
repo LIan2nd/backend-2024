443bb8a4f2fd3048c7cbe0f80b6ac61c09750cef
LIan2nd <alfiannurusyaid19@gmail.com>
2024-12-06 15:59:45 +0700

    pertemuan-10

---
+++ praktikum/callback.js
@@ -0,0 +1,50 @@
+const tambah = (a, b) => {
+  console.log(a + b);
+}
+
+const kurang = (a, b) => {
+  console.log(a - b);
+}
+
+const calculator = (a, b, operator) => {
+  return operator(a, b);
+}
+
+// calculator(10, 5, kurang);
+
+// asynchrounous callback
+
+/**
+ * berenang
+ * bersepeda
+ * berlari
+ */
+
+const berenang = () => {
+  console.log("berenang selesai");
+}
+
+const bersepeda = () => {
+  console.log("bersepeda selesai");
+}
+
+const berlari = () => {
+  console.log("berlari selesai");
+}
+
+const triathlon = () => {
+  setTimeout(() => {
+    berenang();
+
+    setTimeout(() => {
+      bersepeda();
+
+      setTimeout(() => {
+        berlari();
+      }, 4000);
+    }, 2000);
+
+  }, 6000);
+}
+
+triathlon();
---
+++ praktikum/promise.js
@@ -0,0 +1,31 @@
+const berenang = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("berenang selesai.")
+    }, 6000);
+  })
+}
+
+const bersepeda = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("bersepeda selesai.")
+    }, 2000);
+  })
+}
+
+const belari = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("berlari selesai.")
+    }, 4000);
+  })
+}
+
+const triathlon = async () => {
+  console.log(await berenang());
+  console.log(await bersepeda());
+  console.log(await berlari());
+}
+
+triathlon();
